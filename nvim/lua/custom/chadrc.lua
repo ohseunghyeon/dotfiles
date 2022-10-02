19b8c698105559f3aa8e6b1f93db492080b08c01
Oh Seunghyeon <postercolo@gmail.com>
2022-10-02 17:03:52 +0000

    Use nvchad as base

---
+++ nvim/lua/custom/chadrc.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+M.ui = {
+  theme = "gruvbox_material",
+  theme_toggle = { "gruvbox_material", "everforest" }
+}
+
+M.mappings = require "custom.mappings"
+
+M.plugins = require "custom.plugins"
+
+return M
